ab67a30115e8c4f8d47b58c53e0d14c21b2c0d97
slopezvvv <slopez@tuserver.cl>
2019-09-19 01:08:08 +0000

    Initial commit

---
+++ app/src/main/java/cl/tuserver/solemne1/activities/Sumario.java
@@ -0,0 +1,16 @@
+package cl.tuserver.solemne1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cl.tuserver.solemne1.R;
+
+public class Sumario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sumario);
+    }
+}
